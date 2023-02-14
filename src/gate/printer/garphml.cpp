0bed11b0c5ba5244cdc545de2efe105fd8246104
Alexsey Shtokman <alex.sh2002@mail.ru>
2023-02-14 15:10:12 +0300

    Repeated code fragments have been removed, variables have been renamed

--- src/gate/printer/garphml.cpp
+++ src/gate/printer/garphml.cpp
@@ -1,41 +1,60 @@
 #include "graphml.h"
 
+std::string link_deskriptor(Link link)
+{
+	return std::to_string(link.source) + "_" + std::to_string(link.target);
+}
+
+bool link_not_draw(std::set<std::string> &links_draw, Link link)
+{
+	if (links_draw.find(link_deskriptor(link)) == links_draw.end() && links_draw.find(link_deskriptor(link)) == links_draw.end())
+	{
+		links_draw.insert(link_deskriptor(link));
+		links_draw.insert(link_deskriptor(link));
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
 std::ostream &operator<<(const GNet &model, std::ostream &output)
 {
-	output << R"HEADER(<?xml version="1.0" encoding="UTF-8"?>
+	output
+			<< R"HEADER(<?xml version="1.0" encoding="UTF-8"?>
     <graphml xmlns="http://graphml.graphdrawing.org/xmlns">
-    )HEADER";
-	output << "<graph id=\"";
-	output << model.id();
-	output << "\" edgedefault=\"directed\">\n";
+    )HEADER"
+			<< "<graph id=\""
+			<< model.id()
+			<< "\" edgedefault=\"directed\">\n";
 	// Document header
-	auto b = model.gates();
-	std::set<std::string> don;
-	for (long i = 0; i < b.size(); i++)
+	auto &all_gates = model.gates();
+	std::set<std::string> links_draw;
+	for (size_t number_of_gate = 0; number_of_gate < all_gates.size(); number_of_gate++)
 	{
 		output
 				<< "<node id = \""
-				<< b[i]->id()
+				<< all_gates[number_of_gate]->id()
 				<< "\"/>\n";
 		// describing the nodes
-		auto a = b[i]->links();
-		for (long k = 0; k < a.size(); k++)
+		auto &all_links_from_gate = all_gates[number_of_gate]->links();
+		for (size_t number_of_link = 0; number_of_link < all_links_from_gate.size(); number_of_link++)
 		{
-			if (don.find(std::to_string(i) + "_" + std::to_string(k)) == don.end() && don.find(std::to_string(k) + "_" + std::to_string(i)) == don.end()) // we check that we did not draw this edge
+			if (link_not_draw(links_draw, all_links_from_gate[number_of_link])) // we check that we did not draw this edge
 			{
+				std::string link_deskription = link_deskriptor(all_links_from_gate[number_of_link]);
 				output
-						<< "<edge id = \"l" + std::to_string(i) + "_" + std::to_string(k) + "\" source = \""
-						<< a[k].source
+						<< "<edge id = \"l" + link_deskription + "\" source = \""
+						<< all_links_from_gate[number_of_link].source
 						<< "\" target = \""
-						<< a[k].target
+						<< all_links_from_gate[number_of_link].target
 						<< "\">\n"
-						<< "<data key = \"l_d" + std::to_string(i) + "_" + std::to_string(k) + "\">"
-						<< a[k].input
+						<< "<data key = \"l_d" + link_deskription + "\">"
+						<< all_links_from_gate[number_of_link].input
 						<< "</data>\n"
 						<< "</edge>\n";
 				// describing the edges
-				don.insert(std::to_string(i) + "_" + std::to_string(k));
-				don.insert(std::to_string(k) + "_" + std::to_string(i));
 			}
 		}
 	}
--- src/gate/printer/graphml.h
+++ src/gate/printer/graphml.h
@@ -7,5 +7,8 @@
 
 using GNet = eda::gate::model::GNet;
 using Gate = eda::gate::model::Gate;
+using Link = Gate::Link;
 
+std::string link_deskriptor(Link link);
+bool link_not_draw(std::set<std::string> &don, Link link);
 std::ostream &operator<<(const GNet &model, std::ostream &output);
