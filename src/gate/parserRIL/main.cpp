62fa50a059ff85d148f8d13c0b8287af9a8d5108
white_wlf <white_wlf@white.wlf>
2023-03-26 22:33:17 +0300

    Added description of sensetivity list and full operator of the module

--- src/gate/parserRIL/main.cpp
+++ src/gate/parserRIL/main.cpp
@@ -11,20 +11,29 @@
 #include <algorithm>
 
 using namespace eda::gate::model;
+std::map<int, std::string> INPUTS;
+std::map<int, std::string> OUTPUTS;
+std::map<int, std::string > typpfunc;
+std::vector<int> WIRES;
+std::vector<int> seq;
+std::vector<int> clk;
 
-int root;
 void print_modules(const int ind, std::ostream &out, std::ofstream &fout){
     for (const auto &it1 : Yosys::RTLIL::IdString::global_id_index_){
         if (it1.second == ind){
             out << it1.first << " - module of index: " << it1.second << "\n";
         }
+        INPUTS.clear();
+        OUTPUTS.clear();
+        typpfunc.clear();
+        WIRES.clear();
+        seq.clear();
+        clk.clear();
+
     }
 }
 
-std::map<int, std::string> INPUTS;
-std::map<int, std::string> OUTPUTS;
-std::map<int, std::string > typpfunc;
-std::vector<int> WIRES;
+
 
 void print_wires(const Yosys::hashlib::dict<Yosys::RTLIL::IdString, Yosys::RTLIL::Wire*> &wires, std::ostream &out, std::ofstream &fout, eda::gate::model::GNet &net,std::map<unsigned, Gate::Id> &inputs,std::map<unsigned, Gate::Id> &outputs){
     out << "Wires:" << "\n";
@@ -42,6 +51,8 @@ void print_wires(const Yosys::hashlib::dict<Yosys::RTLIL::IdString, Yosys::RTLIL
         width.append(std::to_string(it1->second->width));
         if (it1->second->width>1){
             out<< "    type: bus with width " << it1->second->width << "\n";
+            fout << "output u:" << it1->second->width<< " " << temp << ";\n";
+            OUTPUTS.emplace(index, temp);
         }
         else{
             out << "    type: wire" << "\n";
@@ -83,6 +94,9 @@ std::string function(size_t type){
     if (type==ID($or).index_){
         func = "|";
     }
+    if (type==ID($xor).index_){
+      func="^";
+    }
     return func;
 }
 
@@ -108,7 +122,7 @@ std::string buildRIL(int root){
                 return "(" + buildRIL(CELL.find(root)->second.first) + typpfunc.find(CELL.find(root)->first)->second + buildRIL(CELL.find(root)->second.second) + ")";
             }
             else{
-                return "(" + typpfunc.find(CELL.find(root)->first)->second + buildRIL(CELL.find(root)->second.first) + ")";
+                return typpfunc.find(CELL.find(root)->first)->second+"(" + buildRIL(CELL.find(root)->second.first) + ")";
             }
 
         }
@@ -119,69 +133,6 @@ std::string buildRIL(int root){
             return "(" + INPUTS.find(CELL.find(root)->second.first)->second + typpfunc.find(CELL.find(root)->first)->second + buildRIL(CELL.find(root)->second.second) + ")";
         }
 }
-
-//Gate::Id buildNet(int root,eda::gate::model::GNet &net,std::map<int,GateSymbol> typeFunc,std::map<int, std::pair<int, int>> &cell,std::map<unsigned, Gate::Id> &inputs){
-//    for (auto it: cell){
-//        if (it.second.first==root){
-//            if (typeFunc.find(it.first)->second==GateSymbol::LATCH){
-//                return net.addLatch(inputs.find(it.first)->second,inputs.find(it.second.second)->second);
-//            }
-//            if (typeFunc.find(it.first)->second==GateSymbol::DFF){
-//                return net.addDff(inputs.find(it.first)->second,inputs.find(it.second.second)->second);
-//            }
-//            if(typeFunc.find(it.first)->second==GateSymbol::DFFrs){
-//                int key;
-//                for(auto it1: cell){
-//                    if (it1.second.second==it.first){
-//                        key=it1.first;
-//                    }
-//                }
-//                return net.addDffrs(inputs.find(it.first)->second,inputs.find(cell.find(it.first)->second.second)->second,inputs.find(cell.find(key)->second.first)->second,inputs.find(cell.find(key)->first)->second );
-//            }
-//        }
-//    }
-//    bool flag1=0,flag2=0;
-//    if(inputs.find(cell.find(root)->second.first)!=inputs.end()){
-//        flag1=1;
-//    }
-//    if(inputs.find(cell.find(root)->second.second)!=inputs.end()){
-//        flag2=1;
-//    }
-//    if (flag1==1&&flag2==1){
-//        Gate::SignalList inputs1;
-//        if (cell.find(root)->second.first!=cell.find(root)->second.second){
-//            inputs1.push_back(Gate::Signal::always(inputs.find(cell.find(root)->second.first)->second));
-//            inputs1.push_back(Gate::Signal::always(inputs.find(cell.find(root)->second.second)->second));
-//        }
-//        else{
-//            inputs1.push_back(Gate::Signal::always(inputs.find(cell.find(root)->second.first)->second));
-//        }
-//        return net.addGate(typeFunc.find(root)->second,inputs1);
-//    }
-//    if (flag1==0&&flag2==0){
-//        Gate::SignalList inputs1;
-//        if (cell.find(root)->second.first!=cell.find(root)->second.second){
-//            inputs1.push_back(Gate::Signal::always(buildNet(cell.find(root)->second.first,net,typeFunc,cell,inputs)));
-//            inputs1.push_back(Gate::Signal::always(buildNet(cell.find(root)->second.second,net,typeFunc,cell,inputs)));
-//        }
-//        else{
-//            inputs1.push_back(Gate::Signal::always(buildNet(cell.find(root)->second.first,net,typeFunc,cell,inputs)));
-//        }
-//        return net.addGate(typeFunc.find(root)->second,inputs1);
-//    }
-//    if (flag1==0&&flag2==1){
-//        Gate::SignalList inputs1;
-//        inputs1.push_back(Gate::Signal::always(buildNet(cell.find(root)->second.first,net,typeFunc,cell,inputs)));
-//        inputs1.push_back(Gate::Signal::always(inputs.find(cell.find(root)->second.second)->second));
-//        return net.addGate(typeFunc.find(root)->second,inputs1);
-//    }
-//    if (flag1==1&&flag2==0){
-//        Gate::SignalList inputs1;
-//        inputs1.push_back(Gate::Signal::always(inputs.find(cell.find(root)->second.first)->second));
-//        inputs1.push_back(Gate::Signal::always(buildNet(cell.find(root)->second.second,net,typeFunc,cell,inputs)));
-//        return net.addGate(typeFunc.find(root)->second,inputs1);
-//    }
-//}
 
 void print_cells(const Yosys::hashlib::dict<Yosys::RTLIL::IdString, Yosys::RTLIL::Cell*> &cells, std::ostream &out, std::ofstream &fout, eda::gate::model::GNet &net, std::map<int,GateSymbol> &typeFunc,std::map<int, std::pair<int, int>> &cell){
     out << "Cells:" << "\n";
@@ -237,6 +188,7 @@ void print_cells(const Yosys::hashlib::dict<Yosys::RTLIL::IdString, Yosys::RTLIL
 }
 void print_connections(const std::vector<std::pair<Yosys::RTLIL::SigSpec, Yosys::RTLIL::SigSpec> > &connections, std::ostream &out, std::ofstream &fout, eda::gate::model::GNet &net,std::map<int,GateSymbol> typeFunc,std::map<int, std::pair<int, int>> &cell,std::map<unsigned, Gate::Id> &inputs,std::map<unsigned, Gate::Id> &outputs){
     out << "Connections count: " << connections.size() << "\n";
+    int root;
     for (auto it1 = connections.begin(); it1 != connections.end(); ++it1){
         out << "    Wire " << it1->first.as_wire()->name.index_ << " connects with " << it1->second.as_wire()->name.index_ << "\n";
         if(inputs.find(it1->second.as_wire()->name.index_)==inputs.end()){
@@ -272,8 +224,7 @@ void print_memory(const Yosys::hashlib::dict<Yosys::RTLIL::IdString, Yosys::RTLI
     }
 }
 
-std::vector<int> seq;
-std::vector<int> clk;
+
 
 void print_actions(const std::vector< Yosys::RTLIL::SigSig > &actions, std::ostream &out, std::ofstream &fout){
     int z=0;
@@ -292,14 +243,20 @@ void print_actions(const std::vector< Yosys::RTLIL::SigSig > &actions, std::ostr
             }
 
         }
+        if (it.first.is_wire()) {
+          out << "    nech" << (it.first.as_wire()->name.index_)<< "\n";
+        }
         out << "    ch" << (it.first.is_chunk())<< " "<<(it.second.is_chunk())<< "\n";
         if (it.first.is_chunk() && it.second.is_chunk()){
             out << "    nech" << (it.first.as_chunk().wire)->name.index_<< " "<<(it.second.as_chunk().wire)->name.index_<< "\n";
         }
+        if (it.first.is_chunk()) {
+          out << "    nech" << (it.first.as_chunk().wire->name.index_)<< "\n";
+        }
         out << "    ch" << (it.first.is_fully_const()) <<" "<<(it.second.is_fully_const()) << "\n";
-//        if (it.first.is_fully_const() && it.second.is_fully_const()){
-//            out << "    nech" << (it.first.as_fully_const())<< " "<<(it.second.as_fully_const())<< "\n";
-//        }
+        if (it.first.is_fully_const() && it.second.is_fully_const()){
+            out << "    nech" << (it.first.as_int())<< " "<<(it.second.as_int())<< "\n";
+        }
         out << "    ch" << (it.first.is_fully_def()) <<" "<<(it.second.is_fully_def()) << "\n";
 //        if (it.first.is_fully_def() && it.second.is_fully_def()){
 //            out << "    nech" << (it.first.as_fully_def())<< " "<<(it.second.as_fully_def())<< "\n";
@@ -311,22 +268,59 @@ void print_actions(const std::vector< Yosys::RTLIL::SigSig > &actions, std::ostr
 
     }
 }
+
 void print_syncs(const std::vector<Yosys::RTLIL::SyncRule *> &syncs, std::ostream &out, std::ofstream &fout){
     out << "  syncs\n";
+    std::string st;
     for (auto it1=syncs.begin(); it1 != syncs.end(); ++it1){
-        out << "    type " << (*it1)->type << "\n";
+      std::vector<std::string> state;
+        out << "    type " << sizeof((*it1)->type) << "\n";
+        //enum Student_FirstName{ Denys, Rabbit, Milana, Olexandr };
+            std::string Student_FirstNameString[]{"ST0", // level sensitive: 0
+                  "ST1", // level sensitive: 1
+                  "STp", // edge sensitive: posedge
+                  "STn", // edge sensitive: negedge
+                  "STe", // edge sensitive: both edges
+                  "STa", // always active
+                  "STi"  // init
+            };
+            std::cout << Student_FirstNameString[(*it1)->type] << std::endl;
+            state.push_back(Student_FirstNameString[(*it1)->type]);
+
+            if (Student_FirstNameString[(*it1)->type]=="STn"){
+              st="negedge";
+            }
+            if (Student_FirstNameString[(*it1)->type]=="STp"){
+              st="posedge";
+            }
+            if (Student_FirstNameString[(*it1)->type]=="ST0"){
+              st="level0";
+            }
+            if (Student_FirstNameString[(*it1)->type]=="ST1"){
+              st="level1";
+            }
+
+
         out << "  signal\n";
         out << "    size " << (*it1)->signal.size() << "\n";
         out << "    as_wire index " << (*it1)->signal.as_wire()->name.index_ << "\n";
+        out << "    as_chunk index " << (*it1)->signal.as_chunk().data.size() << "\n";;
         clk.push_back((*it1)->signal.as_wire()->name.index_);
         out << "  actions\n";
         print_actions((*it1)->actions, out, fout);
     }
     out <<"zzzzzzzzzzss" <<INPUTS.size();
-    fout << "\n@(*) if (" << INPUTS.find(clk[0])->second << ") {\n    " << OUTPUTS.find(seq[0])->second << " = " << INPUTS.find(seq[2])->second << ";\n}\n";
+
+
+    if(INPUTS.find(seq[2])!=INPUTS.end()){
+    fout << "\n@(" << st<<"("<<INPUTS.find(clk[0])->second << ")"<<") {\n    " << OUTPUTS.find(seq[0])->second << " = " << INPUTS.find(seq[2])->second << ";\n}\n";
+    }
+    else{
+      fout << "\n@(" << st<<"("<<INPUTS.find(clk[0])->second << ")"<<") {\n    " << OUTPUTS.find(seq[0])->second << " = " << buildRIL(seq[2])<< ";\n}\n";
+    }
 }
 
-std::vector<int> LEFT, RIGHT;
+
 
 void print_processes(const Yosys::hashlib::dict<Yosys::RTLIL::IdString, Yosys::RTLIL::Process*> &processes, std::ostream &out, std::ofstream &fout){
     out << "Processes count: " << processes.size() << "\n";
@@ -334,9 +328,120 @@ void print_processes(const Yosys::hashlib::dict<Yosys::RTLIL::IdString, Yosys::R
         out << "  name " << it1->second->name.index_ << "\n";
         out << "  root_case: \n";
         print_actions((it1->second->root_case).actions, out, fout);
+
+        for (auto &it : (it1->second->root_case).compare){
+         // z++;
+       //   out << z <<"\n";
+          out << "    ch" << (it.is_wire())<< " "<<(it.is_wire())<< "\n";
+          if (it.is_wire() && it.is_wire()){
+              out << "    nech" << (it.as_wire()->name.index_)<< " "<<(it.as_wire()->name.index_)<< "\n";
+              if (OUTPUTS.find((it.as_wire()->name.index_)) != OUTPUTS.end()){
+                  seq.insert(seq.begin(), (it.as_wire()->name.index_));
+              }
+              else{
+                  seq.push_back((it.as_wire()->name.index_));
+                  seq.push_back((it.as_wire()->name.index_));
+              }
+
+          }
+          out << "    ch" << (it.is_chunk())<< " "<<(it.is_chunk())<< "\n";
+          if (it.is_chunk() && it.is_chunk()){
+              out << "    nech" << (it.as_chunk().wire)->name.index_<< " "<<(it.as_chunk().wire)->name.index_<< "\n";
+          }
+          out << "    ch" << (it.is_fully_const()) <<" "<<(it.is_fully_const()) << "\n";
+  //        if (it.first.is_fully_const() && it.second.is_fully_const()){
+  //            out << "    nech" << (it.first.as_fully_const())<< " "<<(it.second.as_fully_const())<< "\n";
+  //        }
+          out << "    ch" << (it.is_fully_def()) <<" "<<(it.is_fully_def()) << "\n";
+  //        if (it.first.is_fully_def() && it.second.is_fully_def()){
+  //            out << "    nech" << (it.first.as_fully_def())<< " "<<(it.second.as_fully_def())<< "\n";
+  //        }
+          out << "    ch" << (it.is_fully_undef()) <<" "<<(it.is_fully_undef()) << "\n";
+  //        if (it.first.is_fully_undef() && it.second.is_fully_undef()){
+  //            out << "    nech" << (it.first.as_fully_undef())<< " "<<(it.second.as_fully_undef())<< "\n";
+  //        }
+
+
+
+        }
+        std::cout << "END COMPARE";
         for (auto &it2 : (it1->second->root_case).switches){
             out << "    " << (it2->signal.as_chunk().wire)->name.index_ << "\n";
+            out << "    " << (it2->cases.size()) << "\n";
+
+           for (auto z: it2->signal.as_chunk().data){
+             std::string Student_FirstNameString[]{
+               "S0" ,
+                     "S1" ,
+                  "Sx", // undefined value or conflict
+                           "Sz" , // high-impedance / not-connected
+                        "Sa", // don't care (used only in cases)
+                           "Sm"   // marker (used internally by some passes)
+             };
+                 std::cout <<"type chunk" <<Student_FirstNameString[z] << std::endl;
+
+           }
+            for (auto k: it2->cases){
+              out << "    compare" << (k->compare.size()) << "\n";
+              for (auto pp: k->compare) {
+                if (pp.is_wire()){
+                  out<< "   wire"<< pp.as_wire()->name.index_<<"\n";
+                }
+                if (pp.as_bool()){
+                  out<< "   bool"<< pp.as_bool()<<"\n";
+                }
+                if (pp.is_fully_const()){
+                  out<<"    int"<<pp.as_int()<<"\n";
+                }
+
+              }
+              out << "    actions" << (k->actions.size()) << "\n";
+              for (auto ll: k->actions){
+                out << "    ch" << (ll.first.is_wire())<< " "<<(ll.second.is_wire())<< "\n";
+                if (ll.first.is_wire() && ll.second.is_wire()){
+                    out << "    nech" << (ll.first.as_wire()->name.index_)<< " "<<(ll.second.as_wire()->name.index_)<< "\n";
+//                    if (OUTPUTS.find((it.first.as_wire()->name.index_)) != OUTPUTS.end()){
+//                        seq.insert(seq.begin(), (it.first.as_wire()->name.index_));
+//                    }
+//                    else{
+//                        seq.push_back((it.first.as_wire()->name.index_));
+//                        seq.push_back((it.second.as_wire()->name.index_));
+//                    }
+
+                }
+                out << "    ch" << (ll.first.is_chunk())<< " "<<(ll.second.is_chunk())<< "\n";
+
+                if (ll.first.is_chunk() && ll.second.is_chunk()){
+                  //  out << "    nech" << (ll.first.as_chunk().wire)->name.index_<< " "<<(ll.second.as_chunk().wire)->name.index_<< "\n";
+                }
+                out << "    ch" << (ll.first.is_fully_const()) <<" "<<(ll.second.is_fully_const()) << "\n";
+        //        if (it.first.is_fully_const() && it.second.is_fully_const()){
+        //            out << "    nech" << (it.first.as_fully_const())<< " "<<(it.second.as_fully_const())<< "\n";
+        //        }
+                out << "    ch" << (ll.first.is_fully_def()) <<" "<<(ll.second.is_fully_def()) << "\n";
+        //        if (it.first.is_fully_def() && it.second.is_fully_def()){
+        //            out << "    nech" << (it.first.as_fully_def())<< " "<<(it.second.as_fully_def())<< "\n";
+        //        }
+                out << "    ch" << (ll.first.is_fully_undef()) <<" "<<(ll.second.is_fully_undef()) << "\n";
+        //        if (it.first.is_fully_undef() && it.second.is_fully_undef()){
+        //            out << "    nech" << (it.first.as_fully_undef())<< " "<<(it.second.as_fully_undef())<< "\n";
+        //        }
+
+
+
+              }
+              out << "    switches"<<(k->switches.size())<<"\n";
+              for (auto l: k->switches){
+                out << "      " << (l->signal.as_chunk().wire)->name.index_ << "\n";
+                out << "      " << (l->cases.size()) << "\n";
+
+
+              }
+
+
+            }
         }
+        std::cout << "END swITCHES";
         print_syncs(it1->second->syncs, out, fout);
         out << "\n";
     }
