4748fd6417cb59af98d8bec49a1fae191069cd56
Alexsey Shtokman <alex.sh2002@mail.ru>
2023-04-05 19:43:43 +0300

    Coment delete

--- test/gate/printer/graphml_test.cpp
+++ test/gate/printer/graphml_test.cpp
@@ -8,6 +8,4 @@ using namespace eda::printer::graphMl;
 TEST(toGraphMlTest,all) {
   GNet test = *makeRand(5, 5);
   std::cout << test;
-  //Выводит не то, что надо, так как оператор вывода для GNet уже перегружен, 
-  //не совсем понимаю как выбрать нужный мне
 }
