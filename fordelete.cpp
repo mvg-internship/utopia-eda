6a5d8f77d3b4da961d141f214c4b804aa6b553a5
Рябов Георгий Дмитриевич <gdryabov@edu.hse.ru>
2023-04-07 19:58:30 +0300

    upd

--- fordelete.cpp
+++ fordelete.cpp
@@ -12,15 +12,22 @@ int main() {
 
     std::cout << "ENTER" << std::endl;
 
-    for(int i = 0; i < 10; i++) {
-        char* text = "iqw";
-        std::cout << sizeof(text);
-        myStruct ms {text, i};
-        vectors.push_back(ms);
-    }
-    for(auto i : vectors) {
-        std::cout << i.first << " " << i.second << std::endl;
-    }
+    char* text1 = "aaa";
+    char* text2 = "aaa";
+    if(text1 == text2) 
+        std::cout << "EQUAL" << std::endl;
+
+
+
+    // for(int i = 0; i < 10; i++) {
+    //     char* text = "iqw";
+    //     std::cout << sizeof(text);
+    //     myStruct ms {text, i};
+    //     vectors.push_back(ms);
+    // }
+    // for(auto i : vectors) {
+    //     std::cout << i.first << " " << i.second << std::endl;
+    // }
 
     return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,17 @@
-i
+
+//     /*find init*/
+//     for(auto i : maps) {
+//         for(auto j : maps) {
+//             int k = 0; 
+//             if(i != j && i.definite == 0 && strlen(yytext) == strlen(i.name) && j.definite == 1) {
+//                 for (int j = 0; j < strlen(yytext); j++) {
+//                         if (i.name[j] == yytext[j])
+//                             k += 1;
+//                     }
+//                     if (k == strlen(yytext)) {
+//                         i.definite = 1; 
+//                     }
+//             }
+//         }
+//     }
+// }
