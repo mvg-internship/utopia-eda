972d08036a5b6ef307e434ebc40939ff6220c1db
alexey <aayagzov@miem.hse.ru>
2023-04-08 22:48:25 +0300

    ArithmeticLibrary was added

--- src/rtl/library/support.cpp
+++ src/rtl/library/support.cpp
@@ -1,3 +1,11 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the Utopia EDA Project, under the Apache License v2.0
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2023 ISP RAS (http://www.ispras.ru)
+//
+//===----------------------------------------------------------------------===//
+
 #include "gate/model/gnet.h"
 #include "rtl/library/flibrary.h"
 
