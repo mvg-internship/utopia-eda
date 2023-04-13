d8f5a073b7073760731f84e42b676b9a41c3a8b3
Alexsey Shtokman <alex.sh2002@mail.ru>
2023-04-13 21:28:09 +0300

    Small changes

--- src/gate/printer/graphml.cpp
+++ src/gate/printer/graphml.cpp
@@ -27,10 +27,10 @@ std::string linkDescriptionReverse(const Link &link) {
   + "_" + std::to_string(link.input);
 }
 
-bool linkDontDraw(std::set<std::string> &linksDraw, const Link &link) {
-  if (linksDraw.find(linkDescription(link)) == linksDraw.end() &&
-      linksDraw.find(linkDescriptionReverse(link)) == linksDraw.end()) {
-    linksDraw.insert(linkDescription(link));
+bool linkDidntDraw(std::set<std::string> &linksDrawn, const Link &link) {
+  if (linksDrawn.find(linkDescription(link)) == linksDrawn.end() &&
+      linksDrawn.find(linkDescriptionReverse(link)) == linksDrawn.end()) {
+    linksDrawn.insert(linkDescription(link));
     return true;
   }
   return false;
@@ -38,70 +38,70 @@ bool linkDontDraw(std::set<std::string> &linksDraw, const Link &link) {
 
 std::ostream &operator<<(std::ostream &output, GNet &model) {
   // Document header
-  output << 
-         R"(<?xml version="1.0" encoding="UTF-8"?>
+  output << R"(<?xml version="1.0" encoding="UTF-8"?>
             <graphml xmlns="http://graphml.graphdrawing.org/xmlns"  
             xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
             xsi:schemaLocation="http://graphml.graphdrawing.org/xmlns 
             http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd">
             )"
-      << "<graph id=\"G"
-      << model.id()
-      << "\" edgedefault=\"directed\">\n";
+         << "<graph id=\"G"
+         << model.id()
+         << "\" edgedefault=\"directed\">\n";
 
   const auto &allGates = model.gates();
-  std::set<std::string> linksDraw;
-  std::set<size_t> gatesDraw;
+  std::set<std::string> linksDrawn;
+  std::set<size_t> gatesDrawn;
   for (auto *const gate: allGates) {
-    //Adding a description of the vertex if it has not been described yet
-    if((gatesDraw.find(gate->id())) == 
-        gatesDraw.end()) {
-      output << 
-                "<node id=\""
+    // Adding a description of the vertex if it has not been described yet
+    if((gatesDrawn.find(gate->id())) == 
+        gatesDrawn.end()) {
+      output << "<node id=\""
              << gate->id()
              << "\"/>\n";
-      gatesDraw.insert(gate->id());
+      gatesDrawn.insert(gate->id());
     }
-    //We describe all the edges belonging to the vertex, 
-    //provided that they have not already been described
+    // Describe all the edges belonging to the vertex, 
+    // provided that they have not already been described
     auto &allLinksFromGate = gate->links();
     for (const auto link: allLinksFromGate) {
-      if (linkDontDraw(linksDraw, link)) {
-        //We describe the edge by talking about its source, destination 
-        //and which input it comes to
-        const std::string link_deskription =
+      if (linkDidntDraw(linksDrawn, link)) {
+        // Describe the edge by talking about its source, destination 
+        // and which input it comes to
+        const std::string link_description =
             linkDescription(link);
-        output <<
-               "<edge id=\"l" + link_deskription + "\" source=\""
-            << link.source
-            << "\" target=\""
-            << link.target
-            << "\">\n"
-            << "<data key=\"l_d" + link_deskription + "\">"
-            << link.input
-            << "</data>\n"
-            << "</edge>\n";
-        //We check whether there are vertices not drawn in the graph, 
-        //but into which the edge comes or from which it comes
-        if((gatesDraw.find(link.source)) == 
-        gatesDraw.end()) {
-          gatesDraw.insert(link.source);
+        output << "<edge id=\"l" 
+               << link_description 
+               << "\" source=\""
+               << link.source
+               << "\" target=\""
+               << link.target
+               << "\">\n"
+               << "<data key=\"l_d" 
+               << link_description 
+               << "\">"
+               << link.input
+               << "</data>\n"
+               << "</edge>\n";
+        // Check whether there are vertices not drawn in the graph, 
+        // but into which the edge comes or from which it comes
+        if((gatesDrawn.find(link.source)) == 
+        gatesDrawn.end()) {
+          gatesDrawn.insert(link.source);
           output << "<node id=\""
                  << link.source
                  << "\"/>\n";
         }
-        if((gatesDraw.find(link.target)) == 
-        gatesDraw.end()) {
-          gatesDraw.insert(link.target);
-          output << 
-                  "<node id=\""
+        if((gatesDrawn.find(link.target)) == 
+        gatesDrawn.end()) {
+          gatesDrawn.insert(link.target);
+          output << "<node id=\""
                  << link.target
                  << "\"/>\n";
         }
       }
     }
   }
-  //End of document
+  // End of document
   output << "</graph>\n"
          << "</graphml>";
   return output;
